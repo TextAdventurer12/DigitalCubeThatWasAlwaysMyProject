74e5eb8d4529cc52b042e19f46dc1ebdc1d3971e
KermitNuggies <50683296+TextAdventurer12@users.noreply.github.com>
2022-09-16 15:13:23 +1200

    this was always my project

--- interface.c
+++ interface.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "interface.h"
 #include "colours.h"
+#include "vector.h"
 
 void displayFace(cube* cube, int face)
 {
@@ -22,4 +23,28 @@ void displayCube(cube* cube)
         displayFace(cube, i);
     }
     printf("-----------------------------------\n");
+}
+
+movePointer charToFunc(char funcType, char prime)
+{
+    char funcNames[6] = { 'R', 'L', 'U', 'D', 'F', 'B' };
+    movePointer susbus[12] = { R, L, U, D, F, B, RP, LP, UP, DP, FP, BP};
+    loop(i, 6)
+    {
+        if (funcType == funcNames[i])
+        {
+            return susbus[i + (prime * 6)];
+        }
+    }
+    return -1;
+}
+movePointerVector stringToFunc(charVector funcType, charVector prime)
+{
+    if (funcType->len != prime->len) exit(2);
+    make_vec(demonWaifus, movePointerVector);
+    loop(i, funcType->len)
+    {
+        push_back(demonWaifus, charToFunc(funcType->arr[i], prime->arr[i]));
+    }
+    return demonWaifus;
 }
--- interface.h
+++ interface.h
@@ -2,8 +2,13 @@
 #define INTERFACE_H
 
 #include "cube.h"
+#include "vector.h"
+
+vector(movePointer);
 
 void displayFace(cube* cube, int face);
 void displayCube(cube* cube);
+movePointer charToFunc(char funcType, char prime);
+movePointerVector stringToFunc(charVector funcTypes, charVector primes);
 
 #endif
